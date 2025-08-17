01dfb04be3a655702822438295df5942ca5687d5
Edwin Ezidhore <edwinezidhorek202@gmail.com>
2025-08-17 13:49:01 +0530

    featured products fetch issue resolved

--- Client/src/Components/UserComponents/Home/Home.jsx
+++ Client/src/Components/UserComponents/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
 
     const getFeatured = () => {
-        axios.get(`http://localhost:3333/api/v2/featured`, {withCredentials :true})
+        axios.get(`${base_url}/api/v2/featured`, {withCredentials :true})
         .then((res) => {
             // console.log("res is",res);
             setNewArrivals(res.data.products);
